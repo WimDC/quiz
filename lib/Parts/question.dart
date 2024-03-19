96aa62c0837c689c1982457abfb5ef25c697fbd8
WimDC <r0894616@student.thomasmore.be>
2024-03-19 19:46:58 +0100

    undo branch

--- lib/Parts/question.dart
+++ lib/Parts/question.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../settings.dart' as settings;
 import 'package:quiz/Parts/answerbutton.dart';
@@ -8,6 +10,58 @@ class Question extends StatelessWidget {
   final Function processAnswer;
 
   const Question(this.questionNumber, this.score, this.processAnswer);
+
+  // Function to shuffle answers and assign correct answer randomly
+  void shuffleAnswers(List<Map<String, dynamic>>? questions) {
+    final Random random = Random();
+
+    if (questions != null) {
+      for (var question in questions) {
+        List<dynamic>? answers = question["answers"];
+        int? correctIndex = question["correctIndex"];
+        if (answers != null &&
+            correctIndex != null &&
+            correctIndex >= 0 &&
+            correctIndex < answers.length) {
+          var correctAnswer = answers[correctIndex];
+
+          // Shuffle the answers list
+          answers.shuffle();
+
+          // Assign the correct answer to a random index within the shuffled list
+          int randomIndex = random.nextInt(answers.length);
+          answers[randomIndex] = correctAnswer;
+          question["correctIndex"] = randomIndex;
+        }
+      }
+    }
+  }
+
+  // Function to shuffle movies and assign correct and incorrect movies randomly
+  void shuffleMovies(List<dynamic> questions, List<dynamic> movies) {
+    final Random random = Random();
+
+    for (var question in questions) {
+      // Shuffle the movies list
+      movies.shuffle();
+
+      // Select a correct movie
+      var correctMovie = movies[random.nextInt(movies.length)];
+
+      // Select some incorrect movies
+      var incorrectMovies = [];
+      while (incorrectMovies.length < 3) {
+        var randomMovie = movies[random.nextInt(movies.length)];
+        if (randomMovie != correctMovie) {
+          incorrectMovies.add(randomMovie);
+        }
+      }
+
+      // Assign correct and incorrect movies to the question
+      question["correctMovie"] = correctMovie;
+      question["incorrectMovies"] = incorrectMovies;
+    }
+  }
 
   Widget makeRowWithAnswerButtons(answerNumber) {
     return Row(
@@ -30,6 +84,11 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+
+    // Call shuffleAnswers and shuffleMovies
+    shuffleAnswers(settings.questions);
+    shuffleMovies(settings.questions, settings.movies);
+
     return ListView(
       children: [
         Row(
